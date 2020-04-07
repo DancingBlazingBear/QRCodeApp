e0f64c343268f05d7cfc2756c8597981523b9142
DancingBlazingBear <olivrmcarth@gmail.com>
2020-04-07 23:51:43 +0300

    Whole project

---
+++ QRCodeApp2/Controllers/UploadFileController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QRCodeApp2.Models;
+using RestSharp;
+
+namespace QRCodeApp2.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Index(IFormFile formFile)
+        {
+            if (formFile.Length < 0 || formFile.Length > 1048576) return BadRequest();
+
+            var filePath = Path.GetTempFileName();
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await formFile.CopyToAsync(stream);
+            }
+
+            var response = ReadQR(filePath);
+            
+            return Ok(response);
+        }
+
+        private string ReadQR(string filePath)
+        {
+            var client = new RestClient("http://api.qrserver.com/v1/read-qr-code/");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-type", "multipart/form-data");
+            request.AddFile("file", filePath);
+            IRestResponse response = client.Execute(request);
+
+            var json = JsonConvert.DeserializeObject<List<RootObject>>(response.Content);
+            var result = json[0].Symbol[0].Data;
+            
+            return result;
+        }
+    }
+}
---
+++ QRCodeApp2/Models/RootObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRCodeApp2.Models
+{
+    public class RootObject
+    {
+        public string Type { get; set; }
+        public List<Symbol> Symbol { get; set; }
+    }
+
+    public class Symbol
+    {
+        public int Seq { get; set; }
+        public string Data { get; set; }
+        public object Error { get; set; }
+    }
+}
